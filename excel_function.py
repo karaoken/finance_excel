cdb9c6eff601e6e2d0c67c8e611312bba137c203
ekkapon kuekkong <ekkapon.kuekkong@gmail.com>
2024-05-23 17:06:22 +0700

    second commit

---
+++ excel_function.py
@@ -0,0 +1,284 @@
+from openpyxl import Workbook, load_workbook, worksheet
+
+
+def get_statement(sheet: worksheet):
+    stmt = sheet['B4'].value
+    if stmt == "Income Statement":
+        return "pl"
+    elif stmt == "Balance Sheet":
+        return "bs"
+    elif stmt == "Cash Flow":
+        return 'cf'
+
+
+def get_symbol(sheet: worksheet):
+    return sheet['B3'].value
+
+
+def get_type(sheet: worksheet):
+    if sheet['B5'].value == "Company":
+        return "Company"
+    elif "Consolidate" in sheet['B5'].value:
+        return "Consolidate"
+    else:
+        return None
+
+
+def delete_last_5_rows(sheet: worksheet):
+    last_row = get_last_row(sheet, 1)
+    sheet.delete_rows(last_row - 4, amount=5)
+
+
+def get_last_row(sheet: worksheet, col):
+    starting_row = 500
+    for row in range(starting_row, 1, -1):
+        if sheet.cell(row=row, column=col).value is not None:
+            return row
+
+
+def get_last_column(sheet: worksheet, row):
+    starting_col = 200
+    for col in range(starting_col, 1, -1):
+        if sheet.cell(row=row, column=col).value is not None:
+            return col
+
+
+def convert_date_format(date):
+    if date[1:7] == "Yearly":
+        year = date[10:14]
+        quarter = "Q4"
+    else:
+        year = date[6:10]
+        quarter = date[1:3]
+    return f"{quarter}/{year}"
+
+
+def change_date_format(sheet: worksheet):
+    last_col = get_last_column(sheet, 1)
+    period_row = find_row_of(" Period", sheet)
+    for col in range(2, last_col + 1):
+        converted_date = convert_date_format(sheet.cell(row=period_row, column=col).value)
+        sheet.cell(row=period_row, column=col).value = converted_date
+        # print(convert_date_format(sheet.cell(row=period_row, column=col).value))
+
+
+def find_row_of(looking_title, sheet: worksheet):
+    for row in range(1, 200):
+        sheet_title = sheet.cell(row=row, column=1).value
+        if sheet_title is None:
+            return None
+        if sheet_title == looking_title:
+            return row
+    return None
+
+
+def add_gp_row(sheet: worksheet):
+    sga_row = find_row_of("    Selling And Administrative Expenses", sheet)
+    cost_row = find_row_of("    Costs", sheet)
+    oper_rev_row = find_row_of("    Revenue From Operations", sheet)
+    last_column = get_last_column(sheet, 1)
+
+    sheet.insert_rows(sga_row, 2)
+    sheet.cell(row=sga_row, column=1).value = "    Gross Profit"
+    sheet.cell(row=sga_row + 1, column=1).value = "    %GPM"
+    for col in range(2, last_column + 1):
+        cost = sheet.cell(row=cost_row, column=col).value
+        oper_rev = sheet.cell(row=oper_rev_row, column=col).value
+
+        # Update GP row
+        sheet.cell(row=sga_row, column=col).value = oper_rev - cost
+        sheet.cell(row=sga_row, column=col).number_format = '#,##0.00'
+
+        # Update GPM row
+        sheet.cell(row=sga_row + 1, column=col).value = 1 - cost / oper_rev
+        sheet.cell(row=sga_row + 1, column=col).number_format = "0.00%"
+
+
+def add_sga_to_sales(sheet: worksheet):
+    sga_row = find_row_of("    Selling And Administrative Expenses", sheet)
+    oper_rev_row = find_row_of("    Revenue From Operations", sheet)
+    last_col = get_last_column(sheet, 1)
+
+    sheet.insert_rows(sga_row + 1)  # insert a row below sga row
+    sheet.cell(row=sga_row + 1, column=1).value = "    % SG&A"  # add title
+    for col in range(2, last_col + 1):
+        sga = sheet.cell(row=sga_row, column=col).value
+        oper_rev = sheet.cell(row=oper_rev_row, column=col).value
+        sheet.cell(row=sga_row + 1, column=col).value = sga / oper_rev
+        sheet.cell(row=sga_row + 1, column=col).number_format = '0.00%'
+
+
+def change_number_format(sheet: worksheet):
+    """ Change number format for a thousand Bath to a million Bath"""
+    last_row = get_last_row(sheet, 1)
+    last_col = get_last_column(sheet, 1)
+
+    for row in range(2, last_row + 1):
+        if "Baht/Share" in sheet.cell(row=row, column=1).value:
+            continue
+
+        for col in range(2, last_col + 1):
+            value = sheet.cell(row=row, column=col).value
+            if value is None or type(value) is str or value == 0:
+                sheet.cell(row=row, column=col).value = ""
+                continue
+            sheet.cell(row=row, column=col).value = value / 1000
+            sheet.cell(row=row, column=col).number_format = '#,##0.00'
+
+
+def add_selling_to_sales(sheet):
+    selling_exp_row = find_row_of("      Selling Expenses", sheet)
+    oper_rev_row = find_row_of("    Revenue From Operations", sheet)
+    last_col = get_last_column(sheet, 1)
+
+    sheet.insert_rows(selling_exp_row + 1)  # insert a row below selling_exp row
+    sheet.cell(row=selling_exp_row + 1, column=1).value = "      % Selling Expenses"
+    for col in range(2, last_col + 1):
+        selling_exp = sheet.cell(row=selling_exp_row, column=col).value
+        oper_rev = sheet.cell(row=oper_rev_row, column=col).value
+        value = selling_exp / oper_rev
+        sheet.cell(row=selling_exp_row + 1, column=col).value = value
+        sheet.cell(row=selling_exp_row + 1, column=col).number_format = '0.00%'
+
+
+def add_admin_to_sales(sheet):
+    admin_exp_row = find_row_of("      Administrative Expenses", sheet)
+    oper_rev_row = find_row_of("    Revenue From Operations", sheet)
+    last_col = get_last_column(sheet, 1)
+
+    sheet.insert_rows(admin_exp_row + 1)  # insert a row below selling_exp row
+    sheet.cell(row=admin_exp_row + 1, column=1).value = "      % Admin Expenses"
+    for col in range(2, last_col + 1):
+        admin_exp = sheet.cell(row=admin_exp_row, column=col).value
+        oper_rev = sheet.cell(row=oper_rev_row, column=col).value
+        value = admin_exp / oper_rev
+        sheet.cell(row=admin_exp_row + 1, column=col).value = value
+        sheet.cell(row=admin_exp_row + 1, column=col).number_format = '0.00%'
+
+
+def add(input1, input2):
+    return input1 + input2
+
+
+def minus(input1, input2):
+    return input1 - input2
+
+
+def divide(input1, input2):
+    return input1 / input2
+
+
+def add_fin_item(items, sheet: worksheet, row_to_insert, position, num_format, title):
+    # insert result row
+    result_row = find_row_of(row_to_insert, sheet)
+    if position == "lower":
+        result_row += 1
+    sheet.insert_rows(result_row)
+    sheet.cell(row=result_row, column=1).value = title
+
+    for index, item in enumerate(items):
+        sheet.cell(row=result_row, column=2 + index).value = item
+
+
+def copy_worksheet(src_sheet: worksheet, dest_sheet: worksheet):
+    for row in src_sheet.iter_rows():
+        for cell in row:
+            dest_sheet.cell(row=cell.row, column=cell.col_idx, value=cell.value)
+
+
+def get_fin_items(sheet: worksheet, item_title) -> list:
+    """
+    Get financial item and return in a list.
+    :param sheet:
+    :param item_title: e.g. Revenue From Operation
+    :return: list of financial item
+    """
+    result = []
+    last_col = get_last_column(sheet, 1)
+    last_row = get_last_row(sheet, 1)
+    for row in range(1, last_row + 1):
+        if sheet.cell(row=row, column=1).value == item_title:       # Found title
+            for col in range(2, last_col + 1):
+                result.append(sheet.cell(row=row, column=col).value)
+            return result
+
+    return result
+
+
+def insert_list_to_excel_range(row, items: list, title, sheet: worksheet, num_format):
+    col = 2
+    sheet.insert_rows(row)
+    sheet.cell(row=row, column=1).value = title
+
+    if not items:   # if item is None or empty
+        print(f"Can not assign items to worksheet: {title}")
+        return
+
+    for index, item in enumerate(items):
+        sheet.cell(row=row, column=col + index).value = item
+        sheet.cell(row=row, column=col + index).number_format = num_format
+
+
+def cf_accumulate_to_quarter(sheet: worksheet):
+    if sheet['A1'].value != " Period":
+        return None
+
+    # get last column
+    last_col = get_last_column(sheet, 1)
+    last_row = get_last_row(sheet, 1)
+    for col in range(2, last_col):  # loop through before last column to prevent getting null for col + 1
+        if int(sheet.cell(row=1, column=col).value[1]) > int(sheet.cell(row=1, column=col + 1).value[1]):
+            for row in range(2, last_row + 1):
+                value1 = sheet.cell(row=row, column=col).value
+                value1 = 0 if value1 == '' else value1
+                value2 = sheet.cell(row=row, column=col + 1).value
+                value2 = 0 if value2 == '' else value2
+                sheet.cell(row=row, column=col).value = value1 - value2 if value1 != value2 else ''
+
+
+def list_operation(input1: list, input2: list, operation) -> list:
+    result = []
+
+    # if len(input1) != len(input2):
+    #     print("inputs length are not match")
+    #     return result
+    if (not input1) or (not input2):    # if one of them is empty or None
+        if input1:      # if input1 is not empty or None.
+            return input1
+        elif input2:    # if input1 is not empty or None.
+            return input2
+    # elif not input1:    # if empty or None
+    #     input1 = [0 for _ in input2]
+    # elif not input2:    # if empty or None
+    #     input2 = [0 for _ in input1]
+
+    if operation == "ADD":
+        for m, n in zip(input1, input2):
+            m = 0 if m == '' else m
+            n = 0 if n == '' else n
+            p = m + n
+            if p == 0:
+                result.append('')
+            else:
+                result.append(p)
+        return result
+    elif operation == "SUB":
+        for m, n in zip(input1, input2):
+            m = 0 if m == '' else m
+            n = 0 if n == '' else n
+            p = m - n
+            if p == 0:
+                result.append('')
+            else:
+                result.append(p)
+        return result
+
+    elif operation == "DIVIDE":
+        for m, n in zip(input1, input2):
+            m = 0 if m == '' else m
+            n = 0 if n == '' else n
+            if n == 0 or m == 0:
+                result.append('')
+            else:
+                result.append(m / n)
+        return result
--- main.py
+++ main.py
@@ -1,80 +1,276 @@
+import openpyxl
 from openpyxl import Workbook, load_workbook, worksheet
+import os
 
-wb = load_workbook(filename="pl_conso.xlsx")
+from financial_title import *
+from excel_function import *
 
-ws = wb["Sheet1"]
+company_available = False
+conso_available = False
 
-print(ws['A1'].value)
+path = 'D:\\OneDrive\\Investment\\Stock\\Stock Data\\DITTO\\SETSmartFiles'
+files = os.listdir(path)
 
+for file in files:
+    if file[-5:].upper() == ".XLSX":
+        inspecting_wb = load_workbook(filename=path + "\\" + file)
+        try:
+            inspecting_ws = inspecting_wb["Sheet1"]
+        except KeyError:
+            continue
+        if get_statement(inspecting_ws) == "pl" and get_type(inspecting_ws) == "Company":  # pl_company
+            src_pl_comp_ws = inspecting_wb["Sheet1"]
+            # dest_pl_comp_ws = inspecting_wb.copy_worksheet(src_pl_comp_ws)
+        elif get_statement(inspecting_ws) == "bs" and get_type(inspecting_ws) == "Company":  # bs_company
+            src_bs_comp_ws = inspecting_wb["Sheet1"]
+        elif get_statement(inspecting_ws) == "cf" and get_type(inspecting_ws) == "Company":  # cf_company
+            src_cf_comp_ws = inspecting_wb["Sheet1"]
 
-def get_statement(sheet: worksheet):
-    stmt = sheet['B4'].value
-    if stmt == "Income Statement":
-        return "pl"
-    elif stmt == "Balance Sheet":
-        return "bs"
-    elif stmt == "Cash Flow":
-        return 'cf'
+        elif get_statement(inspecting_ws) == "pl" and get_type(inspecting_ws) == "Consolidate":  # pl_company
+            src_pl_conso_ws = inspecting_wb["Sheet1"]
+        elif get_statement(inspecting_ws) == "bs" and get_type(inspecting_ws) == "Consolidate":  # bs_company
+            src_bs_conso_ws = inspecting_wb["Sheet1"]
+        elif get_statement(inspecting_ws) == "cf" and get_type(inspecting_ws) == "Consolidate":  # cf_company
+            src_cf_conso_ws = inspecting_wb["Sheet1"]
 
+# if not error, create new Excel file for company financial statement
+try:
+    print(src_pl_comp_ws['B3'].value)
+except NameError:
+    print("No Compapny found")
+else:
+    company_available = True
+    comp_wb = Workbook()
+    pl_comp_sheet = comp_wb.create_sheet(title="PL_Company", index=1)
+    bs_comp_sheet = comp_wb.create_sheet(title="BS_Company", index=2)
+    cf_comp_sheet = comp_wb.create_sheet(title="CF_Company", index=3)
+    copy_worksheet(src_pl_comp_ws, pl_comp_sheet)
+    copy_worksheet(src_bs_comp_ws, bs_comp_sheet)
+    copy_worksheet(src_cf_comp_ws, cf_comp_sheet)
+    comp_wb.remove(comp_wb["Sheet"])
+    comp_wb.save("company_statement.xlsx")
 
-def get_symbol(sheet: worksheet):
-    return sheet['B3'].value
+# if not error, create new Excel file for consolidated financial statement
+try:
+    print(src_pl_conso_ws['B3'].value)
+except NameError:
+    print("No Consolidate found")
+else:
+    conso_available = True
+    conso_wb = Workbook()
+    pl_conso_sheet = conso_wb.create_sheet(title="PL_conso", index=1)
+    bs_conso_sheet = conso_wb.create_sheet(title="BS_conso", index=2)
+    cf_conso_sheet = conso_wb.create_sheet(title="CF_conso", index=3)
+    copy_worksheet(src_pl_conso_ws, pl_conso_sheet)
+    copy_worksheet(src_bs_conso_ws, bs_conso_sheet)
+    copy_worksheet(src_cf_conso_ws, cf_conso_sheet)
+    conso_wb.remove(conso_wb["Sheet"])
+    conso_wb.save("conso_statement.xlsx")
 
+conso_sheets = []
+company_sheets = []
+if company_available:
+    company_sheets.append(pl_comp_sheet)
+    company_sheets.append(bs_comp_sheet)
+    company_sheets.append(cf_comp_sheet)
 
-def get_type(sheet: worksheet):
-    return sheet['B5'].value
+if conso_available:
+    conso_sheets.append(pl_conso_sheet)
+    conso_sheets.append(bs_conso_sheet)
+    conso_sheets.append(cf_conso_sheet)
 
+if conso_available:
+    for sheet in conso_sheets:
+        # delete last 5 rows
+        delete_last_5_rows(sheet)
 
-def delete_last_4_rows(sheet: worksheet):
-    last_row = get_last_row(sheet, 1)
-    sheet.delete_rows(last_row - 3, amount=4)
+        # delete first 12 rows
+        sheet.delete_rows(1, amount=12)
 
+        # add Period title to row 1
+        sheet['A1'].value = " Period"
 
-def get_last_row(sheet: worksheet, col):
-    starting_row = 500
-    for row in range(starting_row, 1, -1):
-        if sheet.cell(row=row, column=col).value is not None:
-            return row
+        # Change date format
+        change_date_format(sheet)
 
+        # Change number format for all cells
+        change_number_format(sheet)
 
-def get_last_column(sheet: worksheet, row):
-    starting_col = 500
-    for col in range(starting_col, 1, -1):
-        if sheet.cell(row=row, column=col).value is not None:
-            return col
+    # change cf from accumulate to quarter
+    cf_accumulate_to_quarter(cf_conso_sheet)
 
+    depreciation = get_fin_items(cf_conso_sheet, DEPRECIATION)
 
-def convert_date_format(date):
-    year = date[6:10]
-    quarter = date[1:3]
-    return f"{quarter}/{year}"
+    oper_rev = get_fin_items(pl_conso_sheet, OPER_REV)
+    cost = get_fin_items(pl_conso_sheet, COST)
+    sga = get_fin_items(pl_conso_sheet, SGA)
+    selling_exp = get_fin_items(pl_conso_sheet, SELLING_EXP)
+    admin_exp = get_fin_items(pl_conso_sheet, ADMIN_EXP)
+    total_cost_exp = get_fin_items(pl_conso_sheet, TOTAL_COST_EXP)
+    net_profit = get_fin_items(pl_conso_sheet, NET_PROFIT)
+    owner_net_profit = get_fin_items(pl_conso_sheet, OWN_NET_PROFIT)
+    ebit = get_fin_items(pl_conso_sheet, EBIT)
+    fin_cost = get_fin_items(pl_conso_sheet, FIN_COST)
+    tax_exp = get_fin_items(pl_conso_sheet, TAX_EXP)
+    short_debt = get_fin_items(bs_conso_sheet, SHORT_DEBT)   # Bank over draft
+    short_borrowing = get_fin_items(bs_conso_sheet, SHORT_BORROWING)     # Short term borrowings
+    long_debt_current = get_fin_items(bs_conso_sheet, LONG_DEBT_CURRENT)     # current portion of long term debt
+    long_debt = get_fin_items(bs_conso_sheet, LONG_DEBT)
 
+    total_short_debt = list_operation(short_debt, short_borrowing, "ADD")
+    total_long_debt = list_operation(long_debt, long_debt_current, "ADD")
+    total_debt = list_operation(total_short_debt, total_long_debt, "ADD")
+    gross_profit = list_operation(oper_rev, cost, "SUB")
+    gpm = list_operation(gross_profit, oper_rev, "DIVIDE")
+    sga_to_sales = list_operation(sga, oper_rev, "DIVIDE")
+    selling_exp_to_sales = list_operation(selling_exp, oper_rev, "DIVIDE")
+    admin_exp_to_sales = list_operation(admin_exp, oper_rev, "DIVIDE")
+    oper_profit = list_operation(gross_profit, sga, "SUB")
+    ebitda = list_operation(ebit, depreciation, "ADD")
+    ebitda_to_sales = list_operation(ebitda, oper_rev, "DIVIDE")
+    npm = list_operation(net_profit, oper_rev, "DIVIDE")
 
-symbol = get_symbol(ws)
-statement_type = get_type(ws)
-statement = get_statement(ws)
+    # npm     # below net profit
+    np_row = find_row_of(NET_PROFIT, pl_conso_sheet)
+    insert_list_to_excel_range(row=np_row + 1, sheet=pl_conso_sheet, items=npm, title=NPM, num_format=PERCENT_FORMAT)
 
-# Copy to new worksheet
-copied_worksheet = wb.copy_worksheet(ws)
-new_sheet_name = f"{symbol}_{statement}_{statement_type}"
-copied_worksheet.title = new_sheet_name
-wb.worksheets.append(copied_worksheet)
+    # EBITDA
+    ebit_row = find_row_of(EBIT, pl_conso_sheet)
+    insert_list_to_excel_range(row=ebit_row, sheet=pl_conso_sheet, items=ebitda, title=EBITDA, num_format=NUMBER)  # upper EBIT
 
-new_sheet = wb[new_sheet_name]
+    # %EBITDA @ lower EBITDA
+    ebitda_row = find_row_of(EBITDA, pl_conso_sheet)
+    insert_list_to_excel_range(row=ebitda_row + 1, sheet=pl_conso_sheet, items=ebitda_to_sales, title=EBITDA_MARGIN, num_format=PERCENT_FORMAT)
 
-delete_last_4_rows(new_sheet)
-new_sheet.delete_rows(1, amount=12)
-print(new_sheet['B1'].value)
-print(convert_date_format(new_sheet['B1'].value))
-print(get_last_column(new_sheet, 1))
+    # % admin expense to sales @ below admin
+    admin_exp_row = find_row_of(ADMIN_EXP, pl_conso_sheet)
+    insert_list_to_excel_range(row=admin_exp_row + 1, sheet=pl_conso_sheet, items=admin_exp_to_sales, title=ADMIN_EXP_TO_SALES, num_format=PERCENT_FORMAT)
 
-# Change date format
-last_col = get_last_column(new_sheet, 1)
-last_row = get_last_row(new_sheet, 1)
-for row in range(1, last_row + 1):
-    print(new_sheet.cell(row=row, column=1).value)
-for c in range(2, last_col + 1):
-    converted_date = convert_date_format(new_sheet.cell(row=1, column=c).value)
-    new_sheet.cell(row=1, column=c).value = converted_date
-    print(convert_date_format(new_sheet.cell(row=1, column=c).value))
-wb.save("pl_conso.xlsx")
+    # % selling expense @ below selling
+    selling_row = find_row_of(SELLING_EXP, pl_conso_sheet)
+    if selling_row is not None:
+        insert_list_to_excel_range(row=selling_row + 1, sheet=pl_conso_sheet, items=selling_exp_to_sales,
+                                   title=SELLING_EXP_TO_SALES, num_format=PERCENT_FORMAT)
+
+    # SGA to sales @ below sga
+    sga_row = find_row_of(SGA, pl_conso_sheet)
+    insert_list_to_excel_range(row=sga_row + 1, sheet=pl_conso_sheet, items=sga_to_sales,
+                               title=SGA_TO_SALES, num_format=PERCENT_FORMAT)
+
+    # GPM @ upper sga
+    sga_row = find_row_of(SGA, pl_conso_sheet)
+    insert_list_to_excel_range(row=sga_row, sheet=pl_conso_sheet, items=gpm, title=GPM, num_format=PERCENT_FORMAT)
+
+    # Gross Profit @ above GPM
+    gpm_row = find_row_of(GPM, pl_conso_sheet)
+    insert_list_to_excel_range(row=sga_row, sheet=pl_conso_sheet, items=gross_profit, title=GROSS_PROFIT,
+                               num_format=NUMBER)
+
+    last_row = get_last_row(sheet=pl_conso_sheet, col=1)
+    insert_list_to_excel_range(row=last_row + 1, sheet=pl_conso_sheet, items=total_debt, title=TOTAL_DEBT,
+                               num_format=NUMBER)
+
+    conso_wb.save(path + "\\" + "conso.xlsx")
+
+if company_available:
+    for sheet in company_sheets:
+        # delete last 5 rows
+        delete_last_5_rows(sheet)
+
+        # delete first 12 rows
+        sheet.delete_rows(1, amount=12)
+
+        # add Period title to row 1
+        sheet['A1'].value = " Period"
+
+        # Change date format
+        change_date_format(sheet)
+
+        # Change number format for all cells
+        change_number_format(sheet)
+
+    # change cf from accumulate to quarter
+    cf_accumulate_to_quarter(cf_comp_sheet)
+
+    depreciation = get_fin_items(cf_comp_sheet, DEPRECIATION)
+
+    oper_rev = get_fin_items(pl_comp_sheet, OPER_REV)
+    cost = get_fin_items(pl_comp_sheet, COST)
+    sga = get_fin_items(pl_comp_sheet, SGA)
+    selling_exp = get_fin_items(pl_comp_sheet, SELLING_EXP)
+    admin_exp = get_fin_items(pl_comp_sheet, ADMIN_EXP)
+    total_cost_exp = get_fin_items(pl_comp_sheet, TOTAL_COST_EXP)
+    net_profit = get_fin_items(pl_comp_sheet, NET_PROFIT)
+    owner_net_profit = get_fin_items(pl_comp_sheet, OWN_NET_PROFIT)
+    ebit = get_fin_items(pl_comp_sheet, EBIT)
+    fin_cost = get_fin_items(pl_comp_sheet, FIN_COST)
+    tax_exp = get_fin_items(pl_comp_sheet, TAX_EXP)
+    short_debt = get_fin_items(bs_comp_sheet, SHORT_DEBT)   # Bank over draft
+    short_borrowing = get_fin_items(bs_comp_sheet, SHORT_BORROWING)     # Short term borrowings
+    long_debt_current = get_fin_items(bs_comp_sheet, LONG_DEBT_CURRENT)     # current portion of long term debt
+    long_debt = get_fin_items(bs_comp_sheet, LONG_DEBT)
+
+    total_short_debt = list_operation(short_debt, short_borrowing, "ADD")
+    total_long_debt = list_operation(long_debt, long_debt_current, "ADD")
+    total_debt = list_operation(total_short_debt, total_long_debt, "ADD")
+    gross_profit = list_operation(oper_rev, cost, "SUB")
+    gpm = list_operation(gross_profit, oper_rev, "DIVIDE")
+    sga_to_sales = list_operation(sga, oper_rev, "DIVIDE")
+    selling_exp_to_sales = list_operation(selling_exp, oper_rev, "DIVIDE")
+    admin_exp_to_sales = list_operation(admin_exp, oper_rev, "DIVIDE")
+    oper_profit = list_operation(gross_profit, sga, "SUB")
+    ebitda = list_operation(ebit, depreciation, "ADD")
+    ebitda_to_sales = list_operation(ebitda, oper_rev, "DIVIDE")
+    npm = list_operation(net_profit, oper_rev, "DIVIDE")
+
+    # npm     # below net profit
+    np_row = find_row_of(NET_PROFIT, pl_comp_sheet)
+    insert_list_to_excel_range(row=np_row + 1, sheet=pl_comp_sheet, items=npm, title=NPM,
+                               num_format=PERCENT_FORMAT)
+
+    # EBITDA
+    ebit_row = find_row_of(EBIT, pl_comp_sheet)
+    insert_list_to_excel_range(row=ebit_row, sheet=pl_comp_sheet, items=ebitda, title=EBITDA,
+                               num_format=NUMBER)  # upper EBIT
+
+    # %EBITDA @ lower EBITDA
+    ebitda_row = find_row_of(EBITDA, pl_comp_sheet)
+    insert_list_to_excel_range(row=ebitda_row + 1, sheet=pl_comp_sheet, items=ebitda_to_sales, title=EBITDA_MARGIN,
+                               num_format=PERCENT_FORMAT)
+
+    # % admin expense to sales @ below admin
+    admin_exp_row = find_row_of(ADMIN_EXP, pl_comp_sheet)
+    insert_list_to_excel_range(row=admin_exp_row + 1, sheet=pl_comp_sheet, items=admin_exp_to_sales,
+                               title=ADMIN_EXP_TO_SALES, num_format=PERCENT_FORMAT)
+
+    # % selling expense @ below selling
+    selling_row = find_row_of(SELLING_EXP, pl_comp_sheet)
+    if selling_row is not None:
+        insert_list_to_excel_range(row=selling_row + 1, sheet=pl_comp_sheet, items=selling_exp_to_sales,
+                                   title=SELLING_EXP_TO_SALES, num_format=PERCENT_FORMAT)
+
+    # SGA to sales @ below sga
+    sga_row = find_row_of(SGA, pl_comp_sheet)
+    insert_list_to_excel_range(row=sga_row + 1, sheet=pl_comp_sheet, items=sga_to_sales,
+                               title=SGA_TO_SALES, num_format=PERCENT_FORMAT)
+
+    # GPM @ upper sga
+    sga_row = find_row_of(SGA, pl_comp_sheet)
+    insert_list_to_excel_range(row=sga_row, sheet=pl_comp_sheet, items=gpm, title=GPM, num_format=PERCENT_FORMAT)
+
+    # Gross Profit @ above GPM
+    gpm_row = find_row_of(GPM, pl_comp_sheet)
+    insert_list_to_excel_range(row=sga_row, sheet=pl_comp_sheet, items=gross_profit, title=GROSS_PROFIT,
+                               num_format=NUMBER)
+
+    last_row = get_last_row(sheet=pl_comp_sheet, col=1)
+    insert_list_to_excel_range(row=last_row + 1, sheet=pl_comp_sheet, items=total_debt, title=TOTAL_DEBT,
+                               num_format=NUMBER)
+
+    comp_wb.save(path + "\\" + "company.xlsx")
+
+# conso_oper_rev = get_fin_items(pl_conso_sheet, OPER_REV)
+# add_fin_item(items=conso_oper_rev, sheet=pl_conso_sheet, row_to_insert=COST, position="lower", num_format=NUMBER,
+#              title="OPER_REV")
+
+# conso_wb.save("conso.xlsx")
